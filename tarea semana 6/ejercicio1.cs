7c1e5de842ec857c59d0112b01fa6beb803c0958
JoseAgarcia2019 <20199056@itla.edu.do>
2020-06-17 03:30:09 +0000

    new

---
+++ tarea semana 6/ejercicio1.cs
@@ -0,0 +1,47 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Random ale = new Random();
+		int num;
+		
+		
+		num = ale.Next(50,100);
+		Console.WriteLine("Su numero aleatorio entre 50 y 100 es " +num);
+		
+		
+		
+		if(num >= 50){
+			
+		Console.WriteLine("Desea generar otro numero ");
+	     string afi = Console.ReadLine();
+			
+			if(afi== "si"){
+				
+				num = ale.Next(50,100);
+		   Console.WriteLine("Su segundo numero aleatorio entre 50 y 100 es " +num);
+		    
+				
+			}	
+			
+			 else if(afi== " no") {
+				 
+				 
+				
+				
+					
+				
+				}
+				
+			}
+			
+
+
+
+
+		
+
+	}
+}
